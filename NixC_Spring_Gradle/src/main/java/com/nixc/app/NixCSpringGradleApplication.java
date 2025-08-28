aad8a213be0a9cd59d8eb6cb5708ee615ed9cdc2
RealNixC-C <john7622@naver.com>
2025-08-28 17:51:45 +0900

    upup

---
+++ NixC_Spring_Gradle/src/main/java/com/nixc/app/NixCSpringGradleApplication.java
@@ -0,0 +1,13 @@
+package com.nixc.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NixCSpringGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NixCSpringGradleApplication.class, args);
+	}
+
+}
